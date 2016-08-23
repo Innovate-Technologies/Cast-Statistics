6b5a171ae45cdb3b06bb99dea2a7fff885d672ff
Maartje Eyskens <meyskens@me.com>
2016-08-23 12:52:09 +0200

    Retry if failed response

--- statistics/events.js
+++ statistics/events.js
@@ -2,12 +2,17 @@ import rest from "restler"
 
 export default async (info) => {
 
-    const getListenerUID = (listenerInfo) => new Promise((resolve, reject) => {
+    const getListenerUID = (listenerInfo) => new Promise((resolve) => {
         rest.postJson(`${info.itframeURL}/cast/statistics/${info.username}/${info.key}/create-session`, listenerInfo, {
             timeout: 100000,
-        }).on("complete", (body) => {
-            resolve(body)
-        }).on("timeout", (err) => {reject(err)})
+        }).on("complete", function (body, response) {
+            if (response.statusCode === 200 || response.statusCode === 204) {
+                return resolve(body)
+            }
+            this.retry(2000)
+        }).on("timeout", function () {
+            this.retry(2000)
+        })
     })
 
     const closeListenerSession = (listenerInfo) => {
@@ -16,15 +21,29 @@ export default async (info) => {
                 if (!uid) {
                     return
                 }
-                rest.postJson(`${info.itframeURL}/cast/statistics/${info.username}/${info.key}/close-session`, { uid })
+                rest.postJson(`${info.itframeURL}/cast/statistics/${info.username}/${info.key}/close-session`, { uid }, {
+                    timeout: 100000,
+                }).on("complete", function (body, response) {
+                    if (response.statusCode !== 200 && response.statusCode !== 204) {
+                        this.retry(2000)
+                    }
+                }).on("timeout", function () {
+                    this.retry(2000)
+                })
             })
         }
     }
 
     const closeAllSessions = () => new Promise((resolve) => {
-        rest.post(`${info.itframeURL}/cast/statistics/${info.username}/${info.key}/close-all-sessions`)
-        .on("complete", () => {
-            resolve()
+        rest.post(`${info.itframeURL}/cast/statistics/${info.username}/${info.key}/close-all-sessions`, {
+            timeout: 100000,
+        }).on("complete", function (body, response) {
+            if (response.statusCode === 200 || response.statusCode === 204) {
+                return resolve(body)
+            }
+            this.retry(2000)
+        }).on("timeout", function () {
+            this.retry(2000)
         })
     })
 
