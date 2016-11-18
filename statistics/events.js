9333a96535759eb0c1678c04489d77d594c09ce3
Maartje Eyskens <meyskens@me.com>
2016-11-18 08:39:47 +0100

    Use local object to store promises

--- statistics/events.js
+++ statistics/events.js
@@ -3,6 +3,8 @@ import _ from "underscore"
 
 export default async (info) => {
 
+    const listenerPromises = {}
+
     const getListenerUID = (listenerInfo) => new Promise((resolve) => {
         rest.postJson(`${info.itframeURL}/cast/statistics/${info.username}/${info.key}/create-session`, listenerInfo, {
             timeout: 100000,
@@ -17,8 +19,8 @@ export default async (info) => {
     })
 
     const closeListenerSession = (listenerInfo) => {
-        if (listenerInfo && listenerInfo.statsPromise) {
-            listenerInfo.statsPromise.then(({ uid }) => {
+        if (listenerInfo && listenerInfo.id && listenerPromises[listenerInfo.id]) {
+            listenerPromises[listenerInfo.id].then(({ uid }) => {
                 if (!uid) {
                     return
                 }
@@ -26,8 +28,9 @@ export default async (info) => {
                     timeout: 100000,
                 }).on("complete", function (body, response) {
                     if (response.statusCode !== 200 && response.statusCode !== 204) {
-                        this.retry(2000)
+                        return this.retry(2000)
                     }
+                    delete listenerPromises[listenerInfo.id]
                 }).on("timeout", function () {
                     this.retry(2000)
                 })
@@ -55,7 +58,8 @@ export default async (info) => {
             return;
         }
         try {
-            _.findWhere(streams.streamListeners[listenerInfo.stream][listenerInfo.id], { id: listenerInfo.id }).statsPromise = getListenerUID(listenerInfo)
+            listenerPromises[listenerInfo.id] = getListenerUID(listenerInfo)
+            // _.findWhere(streams.streamListeners[listenerInfo.stream][listenerInfo.id], { id: listenerInfo.id }).statsPromise = getListenerUID(listenerInfo)
         } catch (error) {
             console.log(error)
         }
