0d951b8da3ab6f8447e8575c1a9e08bc8fb6b34b
Maartje Eyskens <meyskens@me.com>
2017-01-04 14:49:44 +0100

    add proxy on api requests

---
+++ http/statistics.js
@@ -0,0 +1,23 @@
+import proxy from "http-proxy-middleware"
+const info = {
+    itframeURL: config.itframeURL || "https://itframe.innovatete.ch",
+    username: config.username,
+    key: (config.internal.statistics || {}).key,
+}
+export default (app) => {
+    const proxyOptions = {
+        target: info.itframeURL,
+        changeOrigin: true,
+        pathRewrite: {},
+    }
+
+    proxyOptions.pathRewrite[`^/api/statistics/${global.config.apikey}/`] = `/cast/statistics/${info.username}/${info.key}/`
+
+    app.get("/api/statistics/:key/*", (req, res, next) => {
+        if (req.params.key !== global.config.apikey) {
+            return res.status(400).json({error: "Invalid API key"})
+        }
+        return next()
+    })
+    app.use("/api/statistics/:key/*", proxy(proxyOptions));
+}
